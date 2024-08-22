fdcf02d7aa47b5876bc4f1339806584df1c8c031
cedric.ngoune <cedric.ngoune.ext@reforestaction.com>
2024-08-22 19:53:03 +0200

    feat: add dto user and validation pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,52 @@
-import { Body, Controller, Delete, Headers, Ip, Get, Param, Post, Patch, Put, Query } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Headers,
+  Ip,
+  Get,
+  Param,
+  ParseIntPipe,
+  DefaultValuePipe,
+  Post,
+  Patch,
+  Put,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common'
+import { CreateUserDto } from './dtos/create-user.dto'
 
 @Controller('users')
 export class UsersController {
+  @Get('/:id?')
+  getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit)
+    console.log(page)
+    return 'Get request to users'
+  }
 
-    @Get('/:id')
-    getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-        console.log(id)
-        console.log(limit)
-        return "Get request to users"
+  @Post()
+  createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto)
+    return 'Post request to users'
+  }
 
-    }
+  @Patch()
+  updateUsers() {
+    return 'Post request to users'
+  }
 
-    @Post()
-    createUsers(
-        @Body('email') email: any,
-        @Ip() ip: any,
-        @Headers() headers: any) {
-        console.log(email)
-        console.log(headers )
-        console.log(ip)
-        return 'Post request to users'
-    }
+  @Put()
+  modifyUsers() {
+    return 'Post request to users'
+  }
 
-    @Patch()
-    updateUsers() {
-        return 'Post request to users'
-    }
-
-    @Put()
-    modifyUsers() {
-        return 'Post request to users'
-    }
-
-    @Delete()
-    deleteUsers() {
-        return 'Post request to users'
-    }
+  @Delete()
+  deleteUsers() {
+    return 'Post request to users'
+  }
 }
